a2b0cd8a45dc0e6c9883b5b4c2400148562bb83e
init <beadatbeat@gmail.com>
2024-08-14 21:26:35 +0200

    fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/social-card.jpg',
       navbar: {
         title: 'Polkadot Hungary wiki',
         logo: {
